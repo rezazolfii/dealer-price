ec188670ece323ab14df28d3e4063931e75abdc4
reza zolfi <ie.rzlf@gmail.com>
2024-11-26 18:27:50 +0330

    modify filter data frame

--- app.py
+++ app.py
@@ -101,13 +101,13 @@ def load_data():
 # Function to get unique products based on the model
 def get_unique_products(model, data):
     # Filter data based on the model
-    words = [x for x in model.split() if x.isalpha()]
-    numbers = [x for x in model.split() if x.isdigit()]
+    words = [x for x in model.split() ]
+    # numbers = [x for x in model.split() if x.isdigit()]
 
   # Filter the DataFrame based on the words and numbers
-    filtered_data = data[data['product_complete_name'].str.contains(' '.join(words))]
-    for number in numbers:
-       filtered_data = filtered_data[filtered_data['product_complete_name'].str.contains(number)]
+    # filtered_data = data[data['product_complete_name'].str.contains(' '.join(words))]
+    for word in words:
+       filtered_data = data[data['product_complete_name'].str.contains(word, case=False)]
 
     # filtered_data
     # filtered_data = data[data['product_complete_name'].str.contains(model, case=False, na=False)]
